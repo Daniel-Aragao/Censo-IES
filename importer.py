0af4b2b7fa9b8ddf802899d71507f5f23c813f0d
Daniel Aragao <danielfilhoce@gmail.com>
2019-03-31 22:02:50 -0300

    up

--- importer.py
+++ importer.py
@@ -1,19 +1,53 @@
 import csv
+import pandas
 
 
 class Importer:
 
     @staticmethod
-    def import_csv(path, delimiter="|"):
+    def import_csv(path: str, delimiter: str ="|", encoding: str ='iso-8859-14', lines_limit: int = 0):
         lines = []
+        count = 0
 
         with open(path, encoding='iso-8859-14') as file_obj_control:
             reader = csv.DictReader(file_obj_control, delimiter=delimiter)
-
+            
             for row in reader:
                 lines.append(row)
+                
+                count += 1 
+                if lines_limit and count >= lines_limit:
+                    break
+
         
         return lines
+
+    @staticmethod
+    def import_workbook(path):
+        return pandas.Excelfile(path)
+
+    @staticmethod
+    def import_sheet_from_workbook(workbook: pandas.io.excel.Excelfile, sheet_name, header=1):
+        return workbook.parse(sheet_name, header=header)
+    
+    @staticmethod
+    def get_sheet_names_from_workbook(workbook: pandas.io.excel.Excelfile):
+        return workbook.sheet_names
+
+    @staticmethod
+    def import_data_dictionary(path, header=1):
+        excel = Importer.import_workbook(path)
+        sheet_names = Importer.get_sheet_names_from_workbook(excel)
+
+        for sheet_name in sheet_names:
+            sheet = Importer.import_sheet_from_workbook(excel, sheet_name, header=1)
+
+            sheet.columns
+
+
+
+
+
 
 
 
