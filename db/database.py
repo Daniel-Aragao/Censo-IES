f10793d6e052833eddf513b2c7d20b9d34c3b09a
Daniel Aragao <danielfilhoce@gmail.com>
2019-04-08 18:12:19 -0300

    updated procedure and database implementation

--- db/database.py
+++ db/database.py
@@ -9,28 +9,33 @@ class Database:
         self.connector = Connector(access_config)
         self.connector.create_db(check_if_exists=True)
 
-        self.__create_general_struct_table()
+        # self.__create_general_struct_table()
+
         self.__create_structs_tables()
 
-    def __create_general_struct_table(self):
-        exists, tables_names = self.connector.exist_tables(
-            [self.database_config["struct_table_name"]])
+    # def __create_general_struct_table(self):
+    # (add this property in config.json in censo_databases) "struct_table_name": "structure"
+    #     exists, tables_names = self.connector.exist_tables(
+    #         [self.database_config["struct_table_name"]])
 
-        if not exists:
-            connection = self.connector.make_connection()
+    #     if not exists:
+    #         connection = self.connector.make_connection()
 
-            connection.execute("CREATE TABLE " + tables_names[0] + "( "
-                               "id INT AUTO_INCREMENT PRIMARY KEY,"
-                               "data_table_name VARCHAR(255), "
-                               "data_struct_table_name VARCHAR(255) NOT NULL,"
-                               "struct_insertion_date DATE NOT NULL"
-                               ")")
+    #         connection.execute("CREATE TABLE " + tables_names[0] + "( "
+    #                            "id INT AUTO_INCREMENT PRIMARY KEY,"
+    #                            "data_table_name VARCHAR(255), "
+    #                            "data_struct_table_name VARCHAR(255) NOT NULL,"
+    #                            "struct_insertion_date DATE NOT NULL"
+    #                            ")")
 
-            self.connector.close_connection()
+    #         self.connector.close_connection()
 
     def __create_structs_tables(self):
-        struct_tables = [table + "_struct" for table in self.database_config["tables"]]
-        data_tables = [table + "_data" for table in self.database_config["tables"]]
+        struct_suffix = "_struct"
+        data_suffix = "_data"
+
+        struct_tables = [table + struct_suffix for table in self.database_config["tables"]]
+        data_tables = [table + data_suffix for table in self.database_config["tables"]]
 
         struct_exists, struct_tables_names = self.connector.exist_tables(struct_tables)
         data_exists, data_tables_names = self.connector.exist_tables(data_tables)
@@ -39,7 +44,7 @@ class Database:
             connection = self.connector.make_connection()
 
             for table_name in struct_tables_names:
-                connection.execute("CREATE TABLE " + table_name + "("
+                connection.execute("CREATE TABLE " + table_name + struct_suffix + " ("
                                     "id INT AUTO_INCREMENT PRIMARY KEY,"
                                     "field_name VARCHAR(100) UNIQUE NOT NULL,"
                                     "synonymous TEXT NOT NULL,"
@@ -53,7 +58,11 @@ class Database:
                 
         
         if not data_exists:
+            connection = self.connector.make_connection()
+
             for table_name in data_tables_names:
-                # connection = self.connector.make_connection()
-                # self.connector.close_connection()
-                pass
+                connection.execute("CREATE TABLE " + table_name + data_suffix + " ("
+                                    "id INT AUTO_INCREMENT PRIMARY KEY"
+                                    ")")
+
+            self.connector.close_connection()
